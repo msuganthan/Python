b6a47b94e04d306b11125eaa70e70cb6220a3b1d
msuganthan <suganthanchandrika@gmail.com>
2016-07-10 23:34:43 +0530

    python studies

--- VariableAndInputs.py
+++ VariableAndInputs.py
@@ -35,4 +35,61 @@ SyntaxError: invalid syntax
 8
 >>> myVariable
 2
+>>> value = input("Enter the value: ")
+Enter the value: 50
+>>> value
+50
+>>> value +20
+70
+>>> value = input("Enter the value ")
+Enter the value Sugu
+
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    value = input("Enter the value ")
+  File "<string>", line 1, in <module>
+NameError: name 'Sugu' is not defined
+>>> str = input("Enter the values")
+Enter the values Sugu
+
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    str = input("Enter the values")
+  File "<string>", line 1, in <module>
+NameError: name 'Sugu' is not defined
+>>> str = input("Enter the value: ")
+Enter the value: Sugu
+
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    str = input("Enter the value: ")
+  File "<string>", line 1, in <module>
+NameError: name 'Sugu' is not defined
+>>> str = input("Enter the value ")
+Enter the value 'Sugu'
+>>> str
+'Sugu'
+>>> str +30
+
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    str +30
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> str +'Mad'
+'SuguMad'
+>>> str = int(input("Enter a value"))
+Enter a value30
+>>> str
+30
+>>> str = int(input("Enter a value"))
+Enter a value'Sugu'
+
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    str = int(input("Enter a value"))
+ValueError: invalid literal for int() with base 10: 'Sugu'
+>>> str = float(input("Enter a value"))
+Enter a value1
+>>> str
+1.0
 >>> 
