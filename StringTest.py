2511afaa6a5b8d1a7f7b085e0177ba789110f135
msuganthan <suganthanchandrika@gmail.com>
2016-07-11 00:34:10 +0530

    python studies

---
+++ StringTest.py
@@ -0,0 +1,18 @@
+a = 'I am single quoted string \'t'
+b = "I am double quoted string"
+c = """I am triple quoted string"""
+d = '''I too a string'''
+e = 4
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(a + b)
+print(a * 100)
+print(a + str(e))
+
+print(len(a))
+
+print(input("Press Enter to exit"))
