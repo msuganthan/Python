69ad5bf4f2b05f1fc61c824f76e213a329160f7f
msuganthan <suganthanchandrika@gmail.com>
2016-07-10 23:33:12 +0530

    python studies

---
+++ VariableAndInputs.py
@@ -0,0 +1,38 @@
+Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> myVariable
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    myVariable
+NameError: name 'myVariable' is not defined
+>>> myVariable = 30
+>>> myVariable +30
+60
+>>> 
+myVariable = 2
+>>> myVa
+
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    myVa
+NameError: name 'myVa' is not defined
+>>> myVariable
+2
+>>> myVariable ++3
+5
+>>> myVariable +++++5
+7
+>>> myVariable +++++++++++++2
+4
+>>> myVariable +===4334
+SyntaxError: invalid syntax
+>>> myVariable +== 34535
+SyntaxError: invalid syntax
+>>> myVariable
+2
+>>> myVariable ** 3
+8
+>>> myVariable
+2
+>>> 
