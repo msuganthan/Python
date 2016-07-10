aed99b9dd5303081c75db17c7a64d8c54ac02b1a
msuganthan <suganthanchandrika@gmail.com>
2016-07-11 00:05:10 +0530

    python studies

---
+++ test.py
@@ -0,0 +1,7 @@
+x = int(input("Please enter the 1st number"))
+y = int(input("Please enter the 2nd number"))
+z = int(input("Please enter the 3rd number"))
+
+print(max(x, y, z))
+
+input("Please press enter to exit")
