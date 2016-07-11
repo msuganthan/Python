14364cc1e4ab68ea6cf84c44792eae4b55ed3156
msuganthan <suganthanchandrika@gmail.com>
2016-07-12 02:21:22 +0530

    python studies

--- inheritance.py
+++ inheritance.py
@@ -2,10 +2,15 @@ class Parent:
     value1="this is value 1"
     value2="this is value 2"
 
-class Child (Parent):
+
+class Parent1:
+    value1="this is value 1"
+    value2="this is value 2"  
+
+class Child (Parent, Parent1):
     pass
 
 parent_= Parent()
 print(parent_.value1)
 child_ = Child()
-print(child_.value1)
+print(child_.value2)
