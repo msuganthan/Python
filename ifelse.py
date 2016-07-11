534bd6b37c968bc1b97575e59dc23841e224642e
msuganthan <suganthanchandrika@gmail.com>
2016-07-11 22:55:36 +0530

    python studies

---
+++ ifelse.py
@@ -0,0 +1,20 @@
+n = int(input("Number ?"))
+if n < 0:
+    print("The Absolute value of ", n, " is", -n)
+    print("The Absolute value of ", n, " is", -n)
+    print("The Absolute value of ", n, " is", -n)
+    print("The Absolute value of ", n, " is", -n)
+    print("The Absolute value of ", n, " is", -n)
+    print("The Absolute value of ", n, " is", -n)
+else:
+    print("The Absolute value of ", n, " is", n)
+    print("The Absolute value of ", n, " is", n)
+    print("The Absolute value of ", n, " is", n)
+    print("The Absolute value of ", n, " is", n)
+    print("The Absolute value of ", n, " is", n)
+    print("The Absolute value of ", n, " is", n)
+
+sumOfTwo = 50 + 60
+print(sumOfTwo)
+	
+print(input("Press enter to exit"))
