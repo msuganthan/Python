d80c731acb9b2647d6f235d3f5ac561ddb447940
msuganthan <suganthanchandrika@gmail.com>
2016-07-11 22:09:20 +0530

    python studies

---
+++ slicing.py
@@ -0,0 +1,12 @@
+myList = [1, 2, 3, 4, 5, 6, 7, 8]
+print(myList[4: 8])
+print(myList[4:])
+print(myList[:])
+print(myList[:5])
+print(myList[-4:-2])
+print(myList[0:10:2])
+print(myList[0:10:3])
+print(myList[::3])
+print(myList[::-2])
+
+print(input("Press enter to exit"))
