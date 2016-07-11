9d3529988223e759c501a75d8e861077ade48c70
msuganthan <suganthanchandrika@gmail.com>
2016-07-12 02:14:36 +0530

    python studies

--- classes.py
+++ classes.py
@@ -1,11 +1,15 @@
 class Person:
+    def __init__(self):
+        self.id=id
+        print("Our class is created")
+    def __del__(self):
+        print("Our class object is destroyed")
     def setFullName(self, firstName, lastName): #self points to class itself
         self.firstName = firstName
         self.lastName = lastName
-
-    def printFullName(self):
+    def printFullName(self0):
         return (self.firstName + " "+ self.lastName) 
 
-personName=Person()
+personName=Person(1)
 personName.setFullName("SUGU", "M")
 personName.printFullName()
