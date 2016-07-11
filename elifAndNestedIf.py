1ebd87b0858cfe251f33b15d90f4fc37467cf752
msuganthan <suganthanchandrika@gmail.com>
2016-07-11 23:05:57 +0530

    python studies

---
+++ elifAndNestedIf.py
@@ -0,0 +1,14 @@
+name = input("Name ?")
+animal = input("animal ?")
+if name=="mark":
+    if animal=="dog":
+        print("The name entered is ",name)
+    else:
+        print("Else")
+    print("Name entered is Animal")
+elif name=="john":
+    print("The name entered is ",name)
+elif name=="tom":
+    print("The name entered is ",name)
+else:
+    print("the name is invalid")
