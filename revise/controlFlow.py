cd26ff08c9ffd9a40d2f9ba89156c24f12027c21
msuganthan <suganthanchandrika@gmail.com>
2016-08-11 09:15:56 +0530

    contrl flow

--- revise/controlFlow.py
+++ revise/controlFlow.py
@@ -1,3 +1,4 @@
+#if statement
 x = input("Please enter an integer")
 if x < 0:
     print(x, ' is negative number')
@@ -5,3 +6,13 @@ elif x == 0:
     print(' Zero ')
 elif x > 0:
     print(x, ' is a positive number')
+
+
+#for statement
+words = ['cat', 'window', 'defenstrate']
+for w in words:
+    print(w, len(w))
+
+#range functions
+for i in range(3):
+    print(i)
