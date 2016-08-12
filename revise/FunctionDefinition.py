56463e5fae851f3b6be8a24ae7e7d42eeb7d8e88
msuganthan <suganthanchandrika@gmail.com>
2016-08-12 07:14:56 +0530

    keyword arguments

--- revise/FunctionDefinition.py
+++ revise/FunctionDefinition.py
@@ -25,7 +25,7 @@ print('default argument')
 def askOk(prompt, retries=2, remainder='Please try again!!!'):
     while True:
         ok = input(prompt)
-        if ok in ('y', 'ye', 'yes'):
+        if ok in ('y', 'ye', 'yes'): #test whether or not a sequence contain certain word or not
             return True
         if ok in ('n', 'no'):
             return False
@@ -35,3 +35,31 @@ def askOk(prompt, retries=2, remainder='Please try again!!!'):
         print(remainder)
 
 askOk('Do you really want to quit?')
+
+#Important warning: The default value is evaluated only once.
+#This makes a difference when the default is a mutable object such as a list, dictionary
+print('Important warning with mutable object')
+def f(a, L=[]):
+    L.append(a)
+    return L
+
+
+#Keyword Arguments
+print('Functions can also be called using keyword arguments of the form kwarg=value')
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("voltage", voltage)
+    print("state", state)
+    print("action", action)
+    print("type", type)
+
+parrot(1000)                                          # 1 positional argument
+parrot(voltage=1000)                                  # 1 keyword argument
+parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+def function(a):
+    pass
+
+#function(0, a=0)
