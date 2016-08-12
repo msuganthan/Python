06fa27f0f27af114808b346fb4f8dfcf13f8846d
msuganthan <suganthanchandrika@gmail.com>
2016-08-12 06:39:30 +0530

    return list

--- revise/FunctionDefinition.py
+++ revise/FunctionDefinition.py
@@ -4,6 +4,18 @@ def fibano(limit):
     a, b = 0, 1
     while a < limit:
         print(a)
-        a, b = b , a + b;
+        a, b = b , a + b
 
 fibano(200)
+
+#return a list from function
+print('return list')
+def Rfibono(limit):
+    result = []
+    a, b = 0, 1
+    while a < limit:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+Rfibono(200)
