75e790a2a27f60ee8df84bf720d3cbc0f4539263
msuganthan <msuganthan>
2016-08-14 18:02:12 +0530

    Data structures

--- revise/DataStructures.py
+++ revise/DataStructures.py
@@ -58,3 +58,66 @@ print("After append: ",queue)
 queue.popleft()
 queue.popleft()
 print("After pop: ",queue)
+
+print("==========List Comprehensions=========")
+squares = []
+for x in range(10):
+    squares.append(x ** 2)
+print(squares)
+print("==========List Comprehension using lambdas=======")
+squares = list(map(lambda x: x ** 2, range(10)))
+print(squares)
+print("==========Concise List Comprehension========")
+squares = [x ** 2 for x in range(10)]
+print(squares)
+
+#list comprehension consists of brackets containing an expression followed  by a for clause, then zero or more for or if clauses
+print("====List comprehension using nested for====")
+print([(x, y) for x in [1,2,3] for y in [3,1,4] if x != y])
+print([x for x in [1, 2, 3]])
+
+#create a new list with the values doubled
+vec = [-4, -2, 0, 2, 4]
+print("Doubled vector", [x*2 for x in vec])
+print("filter in list", [x for x in vec if x>0])
+print("apply function to all the elements",[abs(x) for x in vec])
+freshfruit = ['   banana', '   loganberry',  'passion fruit']
+print("call strip ", [weapon.strip() for weapon in freshfruit])
+print("create a tuples ", [(x, x**2) for x in range(6)])
+vec = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print("flatten ===>", [num for elem in vec for num in elem])
+
+print("===========COMPLEX OPERATION==========")
+from math import pi
+print("complex operation using pi ",[str(round(pi, i)) for i in range(6)])
+
+
+#ommitting nested operation
+
+print("========DEL STATEMENT=========")
+a = [-1, 1, 66.55, 333, 333, 1234.5]
+print("Before del", a)
+del a[0]
+print("After del a[0]", a)
+del a[2:4]
+print("After del a[2:4]", a)
+del a[:]
+print("After del a[:]", a)
+
+print("========TUPLES=============")
+#tuples consists of number of elements separated by commas
+t = 123, 456, 'hello'
+print("tuple & t[0]", t, t[0])
+print("======NESTED TUPLES========")
+u = t, (1, 2, 3, 4, 5)
+print("nested ", u)
+#tuples are immutable
+
+empty = ()
+singleton = 'hello',    # <-- note trailing comma
+len(empty)
+len(singleton)
+singleton
+x, y, z = t
+
+
