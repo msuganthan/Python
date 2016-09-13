7bf3d6c7e9668fc6230daf4f23460d50496bdc3d
Suganthan <suganthanchandrika@gmail.com>
2016-09-13 11:11:36 +0530

    refactor

--- VariableAndInputs.py
+++ VariableAndInputs.py
@@ -1,6 +1,5 @@
 Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> myVariable
 >>> myVariable = 30
 >>> myVariable +30
 60
